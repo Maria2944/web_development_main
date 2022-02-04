67b1594a70560f32f004bcf91f33304005b7f432
Maria Toms <84319656+Maria2944@users.noreply.github.com>
2022-02-04 19:48:47 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,15 @@
+function changeToPurple(){
+    document.body.style.backgroundColor ="purple";
+}
+
+function changeToPink(){
+    document.body.style.backgroundColor ="rgb(241, 43, 192)";
+}
+
+function changeToBlue(){
+    document.body.style.backgroundColor ="rgb(65, 199, 252)";
+}
+
+function changeToBlack(){
+    document.body.style.backgroundColor ="Black";
+}
